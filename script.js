c641933896cd4f1625aa217aeb8073739b531a6f
Никита Водяков <nikita.vodyakov@gmail.com>
2022-09-17 12:38:52 +0300

    js 39

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-const myNoun = "dog";
-const myAdjective = "big";
-const myVerb = "ran";
-const myAdverb = "quickly";
-
 // Only change code below this line
-const wordBlanks = "The " + myNoun + " was " + myAdjective + " and " + myVerb + " very " + myAdverb; // Change this line
-// Only change code above this line
+const myArray = ["jelly", 5];
