d3c5e5391be0c64a284723ce59ad3b48fd3559e5
Никита Водяков <nikita.vodyakov@gmail.com>
2022-09-21 20:54:51 +0300

    js ...

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-// Only change code below this line
-const myArray = ["jelly", 5];
+function getLength(arr){
+    //return length of arr
+    return arr.length()
+}
+function getFirst(arr){
+  //return the first element of arr
+  return arr.log[1]
+}
+function getLast(arr){
+  //return the last element of arr
+  return arr.log()
+}
+function pushElement(arr){
+  var el=1;
+  return arr.push(1)
+  //push el to arr
+}
+function popElement(arr){
+  //pop an element from arr
+  
+  return arr.pop()
+}
