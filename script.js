7e88344c9b021bb0ccf9fe962f956bf4e2489381
Никита Водяков <nikita.vodyakov@gmail.com>
2022-09-22 17:07:47 +0300

    js 53

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
-function getLength(arr){
-    //return length of arr
-    return arr.length()
+function myLocalScope() {
+  // Only change code below this line
+  let myVar = 5
+  console.log('inside myLocalScope', myVar);
 }
-function getFirst(arr){
-  //return the first element of arr
-  return arr.log[1]
-}
-function getLast(arr){
-  //return the last element of arr
-  return arr.log()
-}
-function pushElement(arr){
-  var el=1;
-  return arr.push(1)
-  //push el to arr
-}
-function popElement(arr){
-  //pop an element from arr
-  
-  return arr.pop()
-}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
